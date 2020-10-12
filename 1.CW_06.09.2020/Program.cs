ad630f51e3b86d16bf8de1765b2a2f8eee8f7d38
Александр Сахатырь <alexandr.010256@gmail.com>
2020-10-13 00:00:12 +0300

    ///

---
+++ 1.CW_06.09.2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.CW_06._09._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
