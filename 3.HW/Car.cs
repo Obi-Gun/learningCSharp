e15aa8d39540900190373fbc2025571fcfb818bd
Александр Сахатырь <alexandr.010@yandex.ru>
2020-10-14 23:33:44 +0300

    [WIP]

---
+++ 3.HW/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.HW
+{
+    internal class Car
+    {
+        private readonly string _model;
+        private CarFrame _carFrame;
+        private Engine _engine;
+        private Transmission _transmission;
+        private int _horsePower;
+    }
+}
