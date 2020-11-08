5bf9472f2a47dd54757fafd4835bb6693e3f9d2a
Александр Сахатырь <alexandr.010@yandex.ru>
2020-11-08 21:28:06 +0300

    HomeWork 7 overriding operators done

---
+++ 7.HW_8.11.2020_override_operators/Money.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7.HW_8._11._2020_override_operators
+{
+    public class Money
+    {
+        private int _cents = 0;
+
+        public int Cents {
+            get { return _cents; }
+            private set 
+            {
+                if (value < 0)
+                    throw new Exception("Cents count can't be less then zero");
+                _cents = value;
+            } 
+        }
+
+        public Money(int dollars, int cents)
+        {
+            if (cents < 0)
+                throw new ArgumentNullException("cents count can't be less than 0");
+            if (dollars < 0)
+                throw new ArgumentNullException("dollars count can't be less than 0");
+            Cents = cents + dollars * 100;
+        }
+
+        public Money(int cents) : this(0, cents) 
+        { }
+
+        private void CheckIsBankrupt()
+        {
+            if (Cents < 0)
+                throw new Exception("client is bankrupt");
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            return new Money(money1.Cents + money2.Cents);
+        }
+
+        public static Money operator -(Money money1, Money money2)
+        {
+            return new Money(money1.Cents - money2.Cents);
+        }
+
+        public static Money operator /(Money money1, int times)
+        {
+            if (times == 0)
+                throw new DivideByZeroException();
+            return new Money(money1.Cents / times);
+        }
+
+        public static Money operator *(Money money1, int times)
+        {
+            return new Money(money1.Cents * times);
+        }
+
+        public static Money operator ++(Money money)
+        {
+            ++money.Cents;
+            return money;
+        }
+
+        public static Money operator --(Money money)
+        {
+            --money.Cents;
+            return money;
+        }
+
+        public static bool operator <(Money money1, Money money2)
+        {
+            return money1.Cents < money2.Cents;
+        }
+
+        public static bool operator >(Money money1, Money money2)
+        {
+            return money1.Cents > money2.Cents;
+        }
+
+        public static bool operator ==(Money money1, Money money2)
+        {
+            return money1.Cents == money2.Cents;
+        }
+
+        public static bool operator !=(Money money1, Money money2)
+        {
+            return money1.Cents != money2.Cents;
+        }
+
+        public override string ToString()
+        {
+            return $"{Cents / 100} dollars {Cents % 100} cents";
+        }
+    }
+}
---
+++ 7.HW_8.11.2020_override_operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _7.HW_8._11._2020_override_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dollars = 13;
+                var cents = 43;
+                var money = new Money(dollars, cents);
+                var money2 = new Money(567, 12);
+
+                Console.WriteLine($"{money2} + {money} = {money2 + money}");
+                Console.WriteLine($"{money2} - {money} = {money2 - money}");
+                Console.WriteLine($"{money2} / {2} = {money2 / 2}");
+                Console.WriteLine($"{money2} * {3} = {money2 * 3}");
+                var money2copy = new Money(money2.Cents);
+                Console.WriteLine($"{money2copy} ++ = {money2++}");
+                var money3copy = new Money(money2.Cents);
+                Console.WriteLine($"{money3copy} -- = {--money2}");
+                Console.WriteLine($"{money2} > {money} = {money2 > money}");
+                Console.WriteLine($"{money2} < {money} = {money2 < money}");
+                Console.WriteLine($"{money2} == {money} = {money2 == money}");
+                Console.WriteLine($"{money2} != {money} = {money2 != money}");
+                var exeptionMoney = new Money(-3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
