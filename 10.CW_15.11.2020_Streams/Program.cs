fa6db33e9de5674c345e210b0b6d2df65e743e92
Александр Сахатырь <alexandr.010@yandex.ru>
2020-11-15 20:16:31 +0300

    10 classwork streams

---
+++ 10.CW_15.11.2020_Streams/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _10.CW_15._11._2020_Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            Task5("10:15 01.05.2020");
+            Task5("10:05 01.05.2020");
+            Task5("10:05       01.05.2020");
+            Task5("10:15 01_05.2020");
+            Task5("10:15 01-05-2020");
+        }
+
+        private static void Task5(string regexStr)
+        {
+            var datePattern = @"[0-2]\d:[0-2]\d +[0-3]\d[\.\-_][0-1]\d[\.\-_][0-9]{4}";
+            var regex = new Regex(datePattern);
+            Console.WriteLine(regex.IsMatch(regexStr) ? "Data received." : "Data is not correct!");
+        }
+
+        private static void Task4()
+        {
+            var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "C__urok_10_1499776005.pdf");
+            var outputFilepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Copy.pdf");
+            var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            var streamOut = new FileStream(outputFilepath, FileMode.Create, FileAccess.Write);
+            var bytesCount = 0;
+            var buf = new byte[1024 * 8];
+            while ((bytesCount = stream.Read(buf, 0, buf.Length)) > 0)
+            {
+                streamOut.Write(buf, 0, bytesCount);
+            }
+            stream.Close();
+            streamOut.Close();
+        }
+
+        private static void Task3()
+        {
+            Console.WriteLine("Enter smth");
+            var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WritedText3.txt");
+            if (!StreamClass.TryWriteBinaryFile(filepath))
+                Console.WriteLine("Can`t write file");
+            if (!StreamClass.TryReadBinaryFile(filepath, out var readedtext))
+                Console.WriteLine("Can`t read file");
+            Console.WriteLine($"Thats was readed {filepath}");
+
+            foreach (var el in readedtext)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+        private static void Task2()
+        {
+            Console.WriteLine("Enter smth");
+            var text = Console.ReadLine().Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WritedText2.txt");
+            
+            if (!StreamClass.TryWriteAllLines(filepath, text))
+                Console.WriteLine("Can`t write file");
+            if (!StreamClass.TryReadAllLines(filepath, out var readedtext))
+                Console.WriteLine("Can`t read file");
+            Console.WriteLine($"Thats was readed {filepath}");
+            foreach(var str in readedtext)
+                Console.WriteLine(str);
+        }
+
+        private static void Task1()
+        {
+            Console.WriteLine("Enter smth");
+            var text = Console.ReadLine();
+            var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WritedText.txt");
+
+            if (!StreamClass.TryWrite(filepath, text))
+                Console.WriteLine("Can`t write file");
+            if (!StreamClass.TryRead(filepath, out var readedtext))
+                Console.WriteLine("Can`t read file");
+            Console.WriteLine(readedtext + filepath);
+        }
+    }
+}
---
+++ 10.CW_15.11.2020_Streams/StreamClass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _10.CW_15._11._2020_Streams
+{
+    public class StreamClass
+    {
+        public static bool TryWrite(string filepath, string text)
+        {
+            try
+            {
+                using var fs = new FileStream(filepath, FileMode.Create);
+                using var sw = new StreamWriter(fs, Encoding.Unicode);
+                sw.WriteLine(text);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool TryRead(string filepath, out string text)
+        {
+            try
+            {
+                using var fs = new FileStream(filepath, FileMode.Open);
+                using var sw = new StreamReader(fs, Encoding.Unicode);
+                text = sw.ReadToEnd();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                text = "";
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool TryReadAllLines(string filepath, out string[] text)
+        {
+            try
+            {
+                text = File.ReadAllLines(filepath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                text = Array.Empty<string>();
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool TryWriteAllLines(string filepath, string[] text)
+        {
+            try
+            {
+                File.WriteAllLines(filepath, text);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool TryReadBinaryFile(string filepath, out List<string> objArr)
+        {
+            objArr = new List<string>();
+            try
+            {
+                using var fs = new FileStream(filepath, FileMode.Open);
+                using var br = new BinaryReader(fs, Encoding.Unicode);
+                objArr.Add(br.ReadString());
+                objArr.Add(br.ReadDouble().ToString());
+                objArr.Add(br.ReadInt32().ToString());
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool TryWriteBinaryFile(string filepath)
+        {
+            try
+            {
+                using var fs = new FileStream(filepath, FileMode.Create);
+                using var bw = new BinaryWriter(fs, Encoding.Unicode);
+
+                string writeText = "sdfgsdf";
+                double pi = 3.1415926;
+                int number = 1256;
+                bw.Write(writeText);
+                bw.Write(pi);
+                bw.Write(number);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+    }
+}
